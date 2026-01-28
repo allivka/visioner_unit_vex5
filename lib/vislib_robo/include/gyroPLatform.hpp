128827f64b92ad7515f51783ae5ff5da7709cca4
allivka <allivka1920@gmail.com>
2026-01-28 19:45:35 +0300

    did stuff

--- lib/vislib_robo/include/gyroPLatform.hpp
+++ lib/vislib_robo/include/gyroPLatform.hpp
@@ -6,14 +6,47 @@ namespace vislib::platform {
 template <typename Controller_t, typename Time_t> class GyroPlatform : public Platform<Controller_t> {
 protected:
     calculators::GyroPidCalculator<Time_t> calculator{};
+    core::UniquePtr<gyro::YawGetter<double>> yawGetter{};
+    core::TimeGetter<Time_t> timeGetter{};
+    double headAngle{};
+    
     
 public:
 
-    double relativeHead{};
     
-    
-    GyroPlatform(const calculators::GyroPidCalculator<Time_t>& calculator, PlatformMotorConfig configuration, size_t parallelismPrecision = 0) noexcept
-    : calculator(calculator), Platform<Controller_t>(configuration, parallelismPrecision) {}
+    GyroPlatform(
+        const calculators::GyroPidCalculator<Time_t>& calculator,
+        core::UniquePtr<gyro::YawGetter<double>>&& yawGetter,
+        core::TimeGetter<Time_t>&& timeGetter,
+        const PlatformMotorConfig& configuration,
+        size_t parallelismPrecision = 0) noexcept
+        : calculator(calculator), yawGetter(core::move(yawGetter)), timeGetter(core::move(timeGetter)), Platform<Controller_t>(configuration, parallelismPrecision) {
+            
+    }
+
+    core::Error go(const double speed, const double angularSpeed = 0, const double speedK = 1, bool isAbsoluteHead = false) noexcept {
+
+        auto time = timeGetter();
+        
+        core::Result<double> angle = yawGetter->getYaw();
+        if(angle.isError()) return angle.error();
+        
+        core::Result<PlatformMotorSpeeds> speeds = calculator.calculateSpeeds(
+            time,
+            angle,
+            headAngle,
+            speed,
+            speedK,
+            angularSpeed
+        );
+        if (speeds.isError()) return speeds.error();
+        
+        core::Error err = this->setSpeeds(speeds());
+        
+        if(err.isError()) return err;
+        
+        return {};
+    }
     
 };
 
--- lib/vislib_robo/include/platform.hpp
+++ lib/vislib_robo/include/platform.hpp
@@ -159,23 +159,21 @@ namespace calculators {
 template<typename TimeType> class GyroPidCalculator {
 public:
     PIDRegulator<double, TimeType>& pid;
-    core::UniquePtr<gyro::YawGetter<double>> getter;
     PlatformMotorConfig config;
 
-    GyroPidCalculator(const PIDRegulator<TimeType>& pid, core::UniquePtr<gyro::YawGetter<double>>&& getter, const PlatformMotorConfig& config) noexcept
-    : pid(pid), getter(core::move(getter)), config(config) { }
+    GyroPidCalculator(const PIDRegulator<TimeType>& pid, const PlatformMotorConfig& config) noexcept
+    : pid(pid), config(config) { }
 
     core::Result<PlatformMotorSpeeds> calculateSpeeds(
         TimeType time,
+        const double dirAngle,
+        const double maintainAngle,
         const motor::Speed& speed,
-        const double speedK = 1,
-        const double angularSpeed = 0
+        const double angularSpeed = 0,
+        const double speedK = 1
     ) noexcept {
-        core::Result<double> angle = getter->getYaw();
-
-        if (angle.isError()) return angle.error();
-
-        return calculatePlatformSpeeds(config, angle, speed, speedK, angularSpeed + pid.compute(angle, time));
+        
+        return calculatePlatformSpeeds(config, dirAngle, speed, speedK, angularSpeed + pid.compute(mainTainAngle, time));
     }
 };
 
